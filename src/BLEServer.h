967e8939ca3a982ffe2baec829a362ca2d4950c3
Laurent Louf <laurent@getmoona.com>
2018-12-21 18:51:33 +0100

    Delete missed rebase artifact : getConnId has changed from private to public member of BLEServer class, so delete the declaration as private.

--- src/BLEServer.h
+++ src/BLEServer.h
@@ -108,7 +108,6 @@ private:
 
 	void            createApp(uint16_t appId);
 	void            deleteApp(void);
-	uint16_t        getConnId();
 	uint16_t        getGattsIf();
 	void            handleGATTServerEvent(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param);
 	void            registerApp(uint16_t);
